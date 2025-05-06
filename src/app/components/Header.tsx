afe60771a8921de6e38b01a5990cf0996f8dfff1
alvaroluis.dev <alvaroluis.dev@gmail.com>
2025-05-05 21:27:03 -0300

    feature/page home

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header(){
     return (
-        <header className="w-full flex justify-around items-center p-2 bg-[#0D201F] shadow-lg">
+        <header className="w-full flex justify-between items-center px-20 py-4 bg-[#0D201F] shadow-lg">
             <p className="font-think text-[#FFFFFF]"> Álvaro Luís</p>
             <nav className="flex gap-12">
                 <a href="#" className="inline-flex items-center text-sm font-think text-[#FFFFFF] hover:text-cyan-500">Home</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 import Header from "./components/Header";
 import ThemeButton from "./components/ThemeButton";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Home() {
   return (
       <main>
         <Header/>
         <ThemeButton/>
-        <div className="flex">
-        <div className="">
-          <h1 className="font-md text-5xl">Álvaro Luís</h1>
-          <p>Desenvolvedor</p>
-          <div className="flex gap-4">
-            <button className="p-4 bg-[#FFFFFF] rounded"></button>
-            <button className="p-4 bg-[#FFFFFF] rounded"></button>
+        <div className="flex justify-between items-center px-48 py-24 flex-wrap">
+
+          <div className="flex flex-col gap-4">
+
+            <h1 className="text-5xl font-semibold">Álvaro Luís</h1>
+            <p className="text-lg text-gray-400">Desenvolvedor</p>
+
+            <div className="flex gap-4">
+              {/* <button className="p-4 bg-[#FFFFFF] rounded"><LinkedInIcon className="text-black" />
+              </button> */}
+              <a className="p-4 bg-[#FFFFFF] rounded" href="https://www.linkedin.com/in/%C3%A1lvarolu%C3%ADs/" target="blank"><LinkedInIcon className="text-black" /></a>
+
+              <a className="p-4 bg-[#FFFFFF] rounded" href="https://github.com/latysno" target="blank"><GitHubIcon className="text-black" /></a>
+
+              {/* <button className="p-4 bg-[#FFFFFF] rounded"><GitHubIcon className="text-black" /></button> */}
+            </div>
+
           </div>
-        </div>
-        <div>
-          <img src="./src/img/068a6d62aebf5e777f0f2833ee6f6c2e.jpg" alt="" />
+
+          <div className="">
+            <img className="w-180 h-80 object-cover rounded-lg shadow-lg border-1 border-gray-600" src="/img/hello_world.gif" alt="" />
+          </div>
+
         </div>
 
+        <div className="text-center">
+          <h2 className="font-normal text-xl">&gt; &nbsp; Meu Portfólio &nbsp; &lt;</h2>
+          <p className="text-2xl py-8"><p>&#123; &nbsp; Aqui você encontra meus projetos, aprendizados e experiências enquanto trilho meu caminho no mundo da tecnologia &nbsp; &#125;</p>
+          </p>
         </div>
       </main>
     
